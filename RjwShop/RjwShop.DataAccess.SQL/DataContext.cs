7274dd10b4f9d759ee7b5328f6c71251137f0c7b
Rich Werning <rjwerning@gmail.com>
2018-08-02 15:58:39 -0500

    Updates for 12

--- RjwShop/RjwShop.DataAccess.SQL/DataContext.cs
+++ RjwShop/RjwShop.DataAccess.SQL/DataContext.cs
@@ -15,5 +15,7 @@ namespace RjwShop.DataAccess.SQL {
 
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductCategory> ProductCategories { get; set; }
+        public DbSet<Basket> Baskets { get; set; }
+        public DbSet<BasketItem> BasketItems { get; set; }
     }
 }
