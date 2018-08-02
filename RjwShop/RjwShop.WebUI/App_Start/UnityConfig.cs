851714b9b16516fc527c6afa433a09f24844c223
Rich Werning <rjwerning@gmail.com>
2018-08-02 14:07:12 -0500

    Updates for 8

--- RjwShop/RjwShop.WebUI/App_Start/UnityConfig.cs
+++ RjwShop/RjwShop.WebUI/App_Start/UnityConfig.cs
@@ -1,6 +1,7 @@
 using RjwShop.Core.Contracts;
 using RjwShop.Core.Models;
 using RjwShop.DataAccess.InMemory;
+using RjwShop.DataAccess.SQL;
 using System;
 
 using Unity;
@@ -45,8 +46,11 @@ namespace RjwShop.WebUI
 
             // TODO: Register your type's mappings here.
             // container.RegisterType<IProductRepository, ProductRepository>();
-            container.RegisterType<IRepository<Product>, InMemoryRepository<Product>>();
-            container.RegisterType<IRepository<ProductCategory>, InMemoryRepository<ProductCategory>>();
+
+            //container.RegisterType<IRepository<Product>, InMemoryRepository<Product>>();
+            //container.RegisterType<IRepository<ProductCategory>, InMemoryRepository<ProductCategory>>();
+            container.RegisterType<IRepository<Product>, SQLRepository<Product>>();
+            container.RegisterType<IRepository<ProductCategory>, SQLRepository<ProductCategory>>();
         }
     }
 }
